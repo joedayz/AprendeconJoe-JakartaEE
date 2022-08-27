ca90c708c3247d8e2197d3ee0bcae6444d9cb927
Jose Diaz <jose.diaz@joedayz.pe>
2022-08-27 16:31:30 -0500

    authorization filter and RequestDispatcher

---
+++ sampleservletjakartaee9/src/main/java/pe/joedayz/jakartaee9/HomeServlet.java
@@ -0,0 +1,28 @@
+package pe.joedayz.jakartaee9;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet1")
+public class HomeServlet extends HttpServlet {
+
+  // Getting request response
+  public void doGet(HttpServletRequest request,
+      HttpServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+    out.print("Welcome to JoeDayz");
+
+    // Closing connections to
+    // avoid any memory leakage
+    out.close();
+  }
+}
